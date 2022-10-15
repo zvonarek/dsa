1524d52fdea6f1858cdce4a1a258b224dd538eea
zvonarek <zvonarek@umich.edu>
2022-10-15 18:31:03 -0400

    started heaps

---
+++ heaps_and_pqueue.py
@@ -0,0 +1,4 @@
+#heaps/priority queue (ordered by p-value)
+#binary heap is min or max depending on pqueue looking for min or max
+#heaps are Binary trees, considered complete: every level is full, except last one
+#
