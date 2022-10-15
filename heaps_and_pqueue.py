51ca08f4e2577bfcc0f773b4b43c6923e3332e29
zvonarek <zvonarek@umich.edu>
2022-10-15 18:52:40 -0400

    working on heaps

--- heaps_and_pqueue.py
+++ heaps_and_pqueue.py
@@ -1,4 +1,6 @@
 #heaps/priority queue (ordered by p-value)
 #binary heap is min or max depending on pqueue looking for min or max
 #heaps are Binary trees, considered complete: every level is full, except last one
-#
+#the order value: recursively, all vals in left are less than root, vice a versa
+#these are just implemented using arrays and dont care about the 0th ind place root at 1
+#left child = 2*i, right child = 2*i + 1 and parent = i//2, only true because complete T
