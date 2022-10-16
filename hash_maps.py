e5822953ed18154f8e1334370efc2679a4e3ab69
zvonarek <zvonarek@umich.edu>
2022-10-16 12:01:55 -0400

    starting on hash maps

---
+++ hash_maps.py
@@ -0,0 +1,12 @@
+#BST implemented sets/maps (dict)
+#simiar to sets but keys are mapped to val and keys can access values
+#hash maps cannot contain duplicates, same with TreeMaps
+#TreeMap HashMap    Operation
+#O(logn) O(1)       Insert
+#O(logn) O(1)       Remove
+#O(logn) O(1)       Search
+#O(n)    O(nlogn)   InOrder
+#Hashmaps don't maintain any type of ordering
+
+
+
