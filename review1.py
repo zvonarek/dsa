38b7b1163d75807a324fdc1a093af9bbafb6aa70
zvonarek <zvonarek@umich.edu>
2022-10-15 20:35:50 -0400

    reviewing

---
+++ review1.py
@@ -0,0 +1,12 @@
+#Time Complexities:
+#Data Structure Access  Search  Insert  Delete
+# Array         O(1)    O(n)    O(n)    O(n)
+# Stack         O(n)    O(n)    O(1)    O(1)
+# Queue         O(n)    O(n)    O(1)    O(1)
+# LList         O(n)    O(n)    O(1)    O(1)
+# Double LList  O(n)    O(n)    O(1)    O(1)
+# Hash Map      N/a     O(1)    O(1)    O(1)
+# BST           O(logn) O(logn) O(logn) O(logn)
+#Worst case Space complexity for all above, M: O(n)
+
+#Linked List
